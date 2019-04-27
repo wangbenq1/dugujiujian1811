7489e5cc74370d9a6cc1b28ee838a69816f480ba
wangbenq1 <1764153673@qq.com>
2019-04-27 22:56:02 +0800

    222222

--- src/main/java/com/atguigu/jianfa1811/DugujiuJianAPP.java
+++ src/main/java/com/atguigu/jianfa1811/DugujiuJianAPP.java
@@ -5,5 +5,8 @@ public class DugujiuJianAPP {
         System.out.println("1111111111111111111111");
         System.out.println("1111111111111111111111");
         System.out.println("1111111111111111111111");
+        System.out.println("2222222222222222222222");
+        System.out.println("2222222222222222222222");
+        System.out.println("2222222222222222222222");
     }
 }
