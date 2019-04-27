cd90b795e74198c877b535a96c3ab0ba0b479278
wangbenq <wbq_916934@163.com>
2019-04-27 22:33:25 +0800

    33333

--- src/main/java/com/atguigu/jianfa1811/DugujiuJianAPP.java
+++ src/main/java/com/atguigu/jianfa1811/DugujiuJianAPP.java
@@ -5,5 +5,12 @@ public class DugujiuJianAPP {
         System.out.println("1111111111111111111111");
         System.out.println("1111111111111111111111");
         System.out.println("1111111111111111111111");
+        System.out.println("3333333333333333333333");
+        System.out.println("3333333333333333333333");
+        System.out.println("3333333333333333333333");
+        System.out.println("3333333333333333333333");
+        System.out.println("3333333333333333333333");
+        System.out.println("3333333333333333333333");
+
     }
 }
